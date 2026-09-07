fa96e156f3e6b1e936a2e816b7f6a906750b81b5
Aldin A Rozario <aldinrozario76@gmail.com>
2026-09-07 13:39:04 +0530

    smoothing(lib) Extract LlmAudit::Error to its own file

--- lib/llm_audit.rb
+++ lib/llm_audit.rb
@@ -1,11 +1,8 @@
 # frozen_string_literal: true
 
 require_relative "llm_audit/version"
+require_relative "llm_audit/error"
 require_relative "llm_audit/severity"
 require_relative "llm_audit/finding"
-
-module LlmAudit
-  class Error < StandardError; end
-end
 
 require_relative "llm_audit/railtie" if defined?(Rails::Railtie)
---
+++ lib/llm_audit/error.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module LlmAudit
+  class Error < StandardError; end
+end
